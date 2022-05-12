5d72ee4d5120161d44cd4963cd17c760fc0ea581
devinjnugraha <devinjn123@gmail.com>
2022-05-12 22:19:07 +0700

    add overlay

--- script.js
+++ script.js
@@ -2,19 +2,37 @@ const input = document.getElementById("input");
 const output = document.getElementById("output");
 const cariTauBtn = document.getElementById("cariTauBtn");
 const card = document.getElementById("card");
+const overlay = document.getElementById("overlay");
 
 function decode() {
-  let inputValue = atob(input.value);
-  output.innerHTML = inputValue;
-  input.style.display = "none";
-  cariTauBtn.style.display = "none";
-  card.style.display = "block";
+  setTimeout(() => {
+    let inputValue = atob(input.value);
+    output.innerHTML = inputValue;
+    input.style.display = "none";
+    cariTauBtn.style.display = "none";
+    card.style.display = "block";
+  }, 300);
+  showOverlay();
 }
 
 function kembali() {
-  input.style.display = "block";
-  cariTauBtn.style.display = "inline-block";
-  input.value = "";
-  input.placeholder = "Your message";
-  card.style.display = "none";
+  setTimeout(() => {
+    input.style.display = "block";
+    cariTauBtn.style.display = "inline-block";
+    input.value = "";
+    input.placeholder = "Your message";
+    card.style.display = "none";
+  }, 300);
+  showOverlay();
+}
+
+function showOverlay() {
+  overlay.classList.toggle("invisible");
+  overlay.classList.toggle("opacity-100");
+  setTimeout(function () {
+    overlay.classList.toggle("opacity-100");
+    setTimeout(() => {
+      overlay.classList.toggle("invisible");
+    }, 300);
+  }, 2000);
 }
