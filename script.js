3740756bb9fbbeebba1fbc148d993d6b0153dbf7
devinjnugraha <devinjn123@gmail.com>
2022-05-12 14:20:49 +0700

    finalisasi check

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const decrypt = () => {
-  let inputValue = atob(document.getElementById("input").value);
-  document.getElementById("valueInput").innerHTML = inputValue;
-};
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const cariTauBtn = document.getElementById("cariTauBtn");
+const card = document.getElementById("card");
+
+function decrypt() {
+  let inputValue = atob(input.value);
+  output.innerHTML = inputValue;
+  input.style.display = "none";
+  cariTauBtn.style.display = "none";
+  card.style.display = "block";
+}
+
+function kembali() {
+  input.style.display = "block";
+  cariTauBtn.style.display = "inline-block";
+  input.value = "";
+  input.placeholder = "Your message";
+  card.style.display = "none";
+}
