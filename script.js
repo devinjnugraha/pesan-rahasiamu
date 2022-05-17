68515bf220906f377eec219f4768adb7e8989ab9
devinjnugraha <devinjn123@gmail.com>
2022-05-18 03:55:50 +0700

    add each line separator feature

--- script.js
+++ script.js
@@ -5,34 +5,45 @@ const card = document.getElementById("card");
 const overlay = document.getElementById("overlay");
 
 function decode() {
-  setTimeout(() => {
-    let inputValue = atob(input.value);
-    output.textContent = inputValue;
-    input.style.display = "none";
-    cariTauBtn.style.display = "none";
-    card.style.display = "block";
-  }, 300);
-  showOverlay();
+    setTimeout(() => {
+        let arrInput = input.value.split(/\n/);
+
+        arrInput.forEach((element) => {
+            if (element === "") {
+                let breaks = document.createElement("br");
+                output.appendChild(breaks);
+            } else {
+                let par = document.createElement("p");
+                par.textContent = atob(element).replaceAll("â", "'");
+                output.appendChild(par);
+            }
+        });
+        input.style.display = "none";
+        cariTauBtn.style.display = "none";
+        card.style.display = "block";
+    }, 300);
+    showOverlay();
 }
 
 function kembali() {
-  setTimeout(() => {
-    input.style.display = "block";
-    cariTauBtn.style.display = "inline-block";
-    input.value = "";
-    input.placeholder = "Your message";
-    card.style.display = "none";
-  }, 300);
-  showOverlay();
+    setTimeout(() => {
+        input.style.display = "block";
+        cariTauBtn.style.display = "inline-block";
+        input.value = "";
+        input.placeholder = "Your message";
+        card.style.display = "none";
+        output.innerHTML = "";
+    }, 300);
+    showOverlay();
 }
 
 function showOverlay() {
-  overlay.classList.toggle("invisible");
-  overlay.classList.toggle("opacity-100");
-  setTimeout(function () {
+    overlay.classList.toggle("invisible");
     overlay.classList.toggle("opacity-100");
-    setTimeout(() => {
-      overlay.classList.toggle("invisible");
-    }, 300);
-  }, 1700);
+    setTimeout(function () {
+        overlay.classList.toggle("opacity-100");
+        setTimeout(() => {
+            overlay.classList.toggle("invisible");
+        }, 300);
+    }, 1700);
 }
